1884a74cc3a7b741f0fe73bc964058e0fd5571c2
atsuyuki <tinju.gren@gmail.com>
2020-08-19 23:25:12 +0900

    Update mcp23017_74hc595_test.ino

--- mcp23017_74hc595_test/mcp23017_74hc595_test.ino
+++ mcp23017_74hc595_test/mcp23017_74hc595_test.ino
@@ -21,7 +21,6 @@ int const SER = 33;
 int const RCLK = 19;
 int const SRCLK = 22;
 
-int keys;
 
 void setup()
 {
@@ -38,6 +37,8 @@ void setup()
     mcp.pinMode(data_line[i], INPUT);
     mcp.pullUp(data_line[i], HIGH);
   }
+  mcp.pinMode(pedal, INPUT);
+  mcp.pullUp(pedal, HIGH);
 
   pinMode(SER, OUTPUT);
   pinMode(RCLK, OUTPUT);
@@ -45,26 +46,52 @@ void setup()
   Serial.begin(115200);
 }
 
+uint32_t pre_keys;
+uint32_t cur_keys;
+uint32_t sus_keys;
+uint32_t out_keys;
+
+uint32_t dif_keys;
+
 void loop()
 {
-  Serial.println(keysRead(), BIN);
-  keysWrite(keysRead());
+  Serial.print(pedal);
+  Serial.println(mcp.digitalRead(pedal));
+
+  pre_keys = cur_keys;
+  cur_keys = keysRead();
+  Serial.print(cur_keys, BIN);
+  Serial.print(" ");
+
+  // サステインペダル処理
+  // ペダルONなら前回値、ペダルOFFなら更新
+  if (!mcp.digitalRead(pedal)) {
+    sus_keys = cur_keys;
+  }
+
+  // ペダルONの間は
+  // out_keys = cur_keys & sus_keys;
+  out_keys = pre_keys & ~cur_keys & ~sus_keys | cur_keys & sus_keys;
+
+  Serial.println(out_keys, BIN);
+  keysWrite(out_keys);
+//  delay(30);
 }
 
 void keysWrite(int keydata)
 {
 
-  for (int j = 32; j > 0; j--)
-  {
-    digitalWrite(RCLK, LOW);
-    shiftOut(SER, SRCLK, LSBFIRST, keydata);
-    shiftOut(SER, SRCLK, LSBFIRST, keydata >> 8);
-    shiftOut(SER, SRCLK, LSBFIRST, keydata >> 16);
-    shiftOut(SER, SRCLK, LSBFIRST, keydata >> 24);
-    //   Serial.println(keydata, BIN);
-    digitalWrite(RCLK, HIGH);
-    //   delay(300);
-  }
+  //  for (int j = 32; j > 0; j--)
+  //  {
+  digitalWrite(RCLK, LOW);
+  shiftOut(SER, SRCLK, LSBFIRST, keydata);
+  shiftOut(SER, SRCLK, LSBFIRST, keydata >> 8);
+  shiftOut(SER, SRCLK, LSBFIRST, keydata >> 16);
+  shiftOut(SER, SRCLK, LSBFIRST, keydata >> 24);
+  //   Serial.println(keydata, BIN);
+  digitalWrite(RCLK, HIGH);
+  //   delay(300);
+  //  }
 }
 
 unsigned int keysRead()
